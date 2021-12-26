c440f26743708a547e8cce96cadf83e8dc5ef535
krakerbrain <marioplantabaja@gmail.com>
2021-12-25 22:58:49 -0300

    Primer Commit. Replantamiento de desafío usando POO

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+import { BuscarAnimal } from "../../bakend/models/BuscarAnimal.js";
+import { data } from "../../bakend/data/assetsAnimales.js";
+
+const animalSeleccionado = document.getElementById("animal");
+const edadAnimal = document.getElementById("edad");
+const button = document.getElementById("btnRegistrar");
+
+const select = () => {
+  animalSeleccionado.addEventListener("change", () => {
+    const animal = animalSeleccionado.options[animalSeleccionado.selectedIndex].value;
+    selectAnimal(animal);
+  });
+  button.addEventListener("click", () => {
+    const animal = animalSeleccionado.options[animalSeleccionado.selectedIndex].value;
+    tarjetaAnimales(animal);
+    sonidoAnimales(animal);
+  });
+};
+select();
+
+const selectAnimal = (animal) => {
+  let animalObject = data.find((data) => data.name == animal);
+  const buscarAnimal = new BuscarAnimal(animalObject.name, animalObject.imagen, animalObject.sonido);
+  buscarAnimal.encontrarAnimal();
+};
+
+const tarjetaAnimales = (animal) => {
+  let animalObject = data.find((data) => data.name == animal);
+  const cardAnimal = new BuscarAnimal(animalObject.name, animalObject.imagen, animalObject.sonido);
+  cardAnimal.tarjetaAnimal();
+};
+const sonidoAnimales = (animal) => {
+  let animalObject = data.find((data) => data.name == animal);
+  const sonidoAnimal = new BuscarAnimal(animalObject.name, animalObject.imagen, animalObject.sonido);
+  sonidoAnimal.sonidosCard();
+};
---
+++ bakend/models/BuscarAnimal.js
@@ -0,0 +1,35 @@
+import { data } from "../data/assetsAnimales.js";
+
+export class BuscarAnimal {
+  constructor(nombre, imagen, sonido) {
+    this.nombre = nombre;
+    this.imagen = imagen;
+    this.sonido = sonido;
+  }
+
+  encontrarAnimal() {
+    const preview = document.getElementById("preview");
+    const img = `<img class = "preview2" src="assets/imgs/${this.imagen}" alt="">`;
+    preview.innerHTML = img;
+    preview.classList.add("previewAnimal");
+  }
+
+  tarjetaAnimal() {
+    const animal = document.getElementById("Animales");
+    const cardAnimal = `
+      <div class="card mr-3" style="width: 15rem">
+        <img src="/assets/imgs/${this.imagen}" class="card-img-top" alt="..." />
+        <div class="card-body bg-secondary">
+          <i id="btnSound" class="far fa-volume-up text-light"><audio id="${this.nombre}-sound" preload="none" src="/assets/sounds/${this.sonido}"></audio></i>
+        </div>
+    </div>
+      `;
+    animal.innerHTML += cardAnimal;
+    //this.sonidoAnimal(this.nombre, this.sonido);
+  }
+
+  sonidosCard() {
+    const player = document.getElementById("player");
+    player.setAttribute("src", `/assets/sounds/${this.sonido}`);
+  }
+}
